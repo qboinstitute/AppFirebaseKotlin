13dfcc1de4bc5f61488752f5249ee7d8b7c764bb
LuisAngel <luisangel.salvat@gmail.com>
2020-09-05 17:38:32 -0500

    3ra. actualización.

--- app/src/main/java/qboinstitute/com/appfirebasekotlin/LoginActivity.kt
+++ app/src/main/java/qboinstitute/com/appfirebasekotlin/LoginActivity.kt
@@ -5,7 +5,15 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.tasks.Task
+import com.google.firebase.auth.AuthCredential
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
 import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
@@ -33,9 +41,50 @@ class LoginActivity : AppCompatActivity() {
                 }
             }
         }
-
-
+        //Autenticacion con Gmail.
+        btnlogingoogle.setOnClickListener {
+            pbautenticacion.visibility = View.VISIBLE
+            val configlogin = GoogleSignInOptions
+                .Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build()
+            val cliente: GoogleSignInClient = GoogleSignIn.getClient(this, configlogin)
+            startActivityForResult(cliente.signInIntent, 777)
+        }
     }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(requestCode == 777){
+            val task: Task<GoogleSignInAccount>
+            = GoogleSignIn.getSignedInAccountFromIntent(data)
+            try{
+                val cuenta : GoogleSignInAccount? = task.getResult(ApiException::class.java)
+                if(cuenta != null){
+                    val credencial : AuthCredential = GoogleAuthProvider
+                        .getCredential(cuenta.idToken, null)
+                    FirebaseAuth.getInstance().signInWithCredential(credencial)
+                        .addOnCompleteListener {
+                            if(it.isSuccessful){
+                                ingresarAlApp(
+                                    cuenta.email.toString(),
+                                    TipoAutenticacion.GOOGLE.name,
+                                    cuenta.displayName.toString(),
+                                    cuenta.photoUrl.toString()
+                                )
+                            }else{
+                                alertarError()
+                            }
+                        }
+                }
+            }catch (e : ApiException){
+                alertarError()
+            }
+
+        }
+    }
+
 
     private fun alertarError() {
         pbautenticacion.visibility = View.GONE
