eb57179cacbd97182a2f54611161c5998d8fbc4d
LuisAngel <luisangel.salvat@gmail.com>
2020-09-05 20:17:03 -0500

    5ta. actualización.

--- app/src/main/java/qboinstitute/com/appfirebasekotlin/ui/RegistroFragment.kt
+++ app/src/main/java/qboinstitute/com/appfirebasekotlin/ui/RegistroFragment.kt
@@ -5,56 +5,60 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.Toast
+import com.google.android.material.textfield.TextInputEditText
+import com.google.firebase.firestore.FirebaseFirestore
 import qboinstitute.com.appfirebasekotlin.R
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [RegistroFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class RegistroFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    lateinit var firestore : FirebaseFirestore
+    lateinit var etnompersona: TextInputEditText
+    lateinit var etapepersona: TextInputEditText
+    lateinit var etedadpersona: TextInputEditText
+    lateinit var btnregistrar: Button
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_registro, container, false)
+        val view : View = inflater.inflate(R.layout.fragment_registro, container, false)
+        etnompersona = view.findViewById(R.id.etnompersona)
+        etapepersona = view.findViewById(R.id.etapepersona)
+        etedadpersona = view.findViewById(R.id.etedadpersona)
+        btnregistrar = view.findViewById(R.id.btnregistrar)
+        firestore = FirebaseFirestore.getInstance()
+        btnregistrar.setOnClickListener {
+            if(etnompersona.text?.isNotEmpty()!! &&
+                etapepersona.text?.isNotEmpty()!! &&
+                etedadpersona.text?.isNotEmpty()!!
+                    ){
+                registrarPersona()
+            }
+        }
+        return view
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment RegistroFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            RegistroFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+    private fun registrarPersona() {
+        val persona = hashMapOf(
+            "apellido" to etapepersona.text.toString(),
+            "nombre" to etnompersona.text.toString(),
+            "edad" to etedadpersona.text.toString().toInt()
+        )
+        firestore.collection("Persona")
+            .add(persona)
+            .addOnSuccessListener { documentReference ->
+                Toast.makeText(context,
+                "El ID del registro es: ${documentReference.id}",
+                Toast.LENGTH_LONG).show()
+            }
+            .addOnFailureListener { e ->
+
             }
     }
+
+
 }
